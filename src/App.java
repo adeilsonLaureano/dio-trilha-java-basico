c8a9bcaafed5e6422a79a5fc7382a106e9538579
adeilsonLaureano <adeilson.dede2018@gmail.com>
2024-04-27 20:59:25 -0300

    feat: commit

---
+++ src/App.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da Agência !");
+        int conta = scanner.nextInt();
+        System.out.println("Por favor, digite a Agência !");
+        String agencia = scanner.next();
+        System.out.println("Por favor, digite o nome !");
+        String nomeCliente = scanner.next();
+        System.out.println("Por favor, digite o saldo !");
+        double saldo = scanner.nextDouble();
+        
+        ContaTerminal contaTerminal = new ContaTerminal(conta, agencia, nomeCliente, saldo);
+
+        contaTerminal.exibirMensagem();
+
+    }
+}
